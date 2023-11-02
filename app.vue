7896e3997139fa4141c7ba1966d1418c5504f868
SHΛPΞ <40563980+shapedigital@users.noreply.github.com>
2023-11-02 10:50:06 +0000

    Adding modules

--- app.vue
+++ app.vue
@@ -1,9 +1,17 @@
 <template>
-  <section class="p-4">
-   
+  <section class="m-8">
+    <div class="p-4">
+    <header class="m-b-4">
+      <h1 class="">
+        Section title
+      </h1>
+    </header>
     <div>
       hello
     </div>
+    <footer class="m-t-8">
 
+    </footer>
+  </div>
   </section>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@unocss/nuxt'],
+  modules: ['@unocss/nuxt', '@nuxtjs/eslint-module', '@pinia/nuxt'],
   devtools: { enabled: true },
   css: ['@unocss/reset/eric-meyer.css'],
+  typescript: {
+    typeCheck: true
+  },
+  eslint: {}
 });
